3f1e0f458dcb214f0e3a1dbc4b76dd24dbba5341
Hoàng Cao Phi <hoangcaophi11061999@gmail.com>
2024-04-13 22:29:20 +0700

    update

--- Test.AES/Client/aes.js
+++ Test.AES/Client/aes.js
@@ -1,17 +1,25 @@
-const crypto = require('crypto')
 var CryptoJS = require("crypto-js");
-const { encode } = require('querystring');
-
 var data = {
     "Name": "Hoang Cao Phi",
     "Action": "Test"
+}
+const base64ToArrayBuffer = (base64) => {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
 }
 
 const enCryptData = async (data) => {
     const jsonDataString = JSON.stringify(data);
 
-    let iv = crypto.randomBytes(16);
-    let secret_key = crypto.randomBytes(32);
+    // let iv = crypto.randomBytes(16);
+    // let secret_key = crypto.randomBytes(32);
+ 
+    let iv = base64ToArrayBuffer("eWe3W5qvFIuJSt07WKaaVw==")
+    let secret_key = base64ToArrayBuffer("47N77QrH/IfmNjKhh49I94/fV/PQbCFG8fmnlzOpiv0=")
 
     iv = CryptoJS.lib.WordArray.create(iv)
     secret_key = CryptoJS.lib.WordArray.create(secret_key)
--- Test.AES/Test.AES/Controllers/WeatherForecastController.cs
+++ Test.AES/Test.AES/Controllers/WeatherForecastController.cs
@@ -1,29 +1,40 @@
-using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace Test.AES.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AESController : ControllerBase
-    { 
-        [HttpPost(Name = "DecryptionData")]
+    public class DemoAesController : ControllerBase
+    {
+        [HttpPost("/DecryptionData")]
         public object DecryptionData([FromBody] Dictionary<string, string> requestData)
         {
             var cipherText = Convert.FromBase64String(requestData["Data"]);
             var iv = Convert.FromBase64String(requestData["Iv"]);
             var key = Convert.FromBase64String(requestData["Key"]);
- 
+
             return DecryptPayload(cipherText, iv, key);
         }
 
-        static string DecryptPayload(byte[] cipherText, byte[] IV, byte[] Key)
+        [HttpGet("/GetIvAndSecret")]
+        public object CreateIvAndKey()
+        {
+            using (Aes myAes = Aes.Create())
+            {
+                return new
+                {
+                    IV = Convert.ToBase64String(myAes.IV),
+                    Key = Convert.ToBase64String(myAes.Key)
+                };
+            }
+        }
+
+        private string DecryptPayload(byte[] cipherText, byte[] IV, byte[] Key)
         {
             if (cipherText == null || cipherText.Length <= 0)
                 throw new ArgumentNullException("cipherText");
- 
+
             string plaintext = null;
 
             using (Aes aesAlg = Aes.Create())
